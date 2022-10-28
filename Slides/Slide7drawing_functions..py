6be11b394a56c380d913f1a3e0a4d87ede59db28
wiserenn <akincilareren@gmail.com>
2022-10-29 02:17:47 +0300

    modified:   Slides/Slide7drawing_functions..py

--- Slides/Slide7drawing_functions..py
+++ Slides/Slide7drawing_functions..py
@@ -1,14 +1,16 @@
 from DrawingPanel import *
 panel = DrawingPanel(400, 400, background="gray")
 
+
 def worm():
     for i in range(1, 11):
         panel.fill_oval(100+20*i, 5+20*i, 50, 50, "red")
 
- 
+
 def circles():
     for i in range(1, 11):
         panel.draw_oval(100, 100, 22*i, 22*i, "magenta")
+
 
 def draw_car():
     panel.fill_rect(10, 30, 100, 50, "black")
@@ -16,4 +18,18 @@ def draw_car():
     panel.fill_oval(80, 70, 20, 20, "red")
     panel.fill_rect(80, 40, 30, 20, "cyan")
 
-draw_car()
+
+def my_function_top():
+    for i in range(0, 20):
+        panel.draw_rectangle(20, 20+10*i, 200-10*i, 10)
+
+
+def my_function_left():
+    for i in range(0, 10):
+        panel.draw_rectangle(20+10*i, 20+10 * i, 100-10*i, 10)
+
+def my_function_bottom():
+    for i in range(0, 10):
+        panel.draw_rectangle(110-10*i, 20+10 * i, 10+10*i, 10)
+        
+my_function_bottom()
