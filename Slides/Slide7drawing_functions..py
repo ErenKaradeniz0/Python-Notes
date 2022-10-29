26dc50e15260788a7a4695f84b9b9ab7b11a4430
wiserenn <akincilareren@gmail.com>
2022-10-29 21:18:25 +0300

    modified:   Slides/Slide10.py 	new file:   Slides/Slide11.py 	new file:   Slides/Slide12.py 	new file:   Slides/Slide13.py 	new file:   Slides/Slide14.py 	new file:   Slides/Slide15.py 	modified:   Slides/Slide7drawing_functions..py 	modified:   Slides/Slide8.py 	modified:   Slides/Slide9.py

--- Slides/Slide7drawing_functions..py
+++ Slides/Slide7drawing_functions..py
@@ -1,35 +1,62 @@
 from DrawingPanel import *
-panel = DrawingPanel(400, 400, background="gray")
 
 
 def worm():
+    panel = DrawingPanel(260, 200, background="light gray")
     for i in range(1, 11):
         panel.fill_oval(100+20*i, 5+20*i, 50, 50, "red")
 
 
 def circles():
+    panel = DrawingPanel(260, 200, background="light gray")
     for i in range(1, 11):
         panel.draw_oval(100, 100, 22*i, 22*i, "magenta")
 
 
-def draw_car():
-    panel.fill_rect(10, 30, 100, 50, "black")
-    panel.fill_oval(20, 70, 20, 20, "red")
-    panel.fill_oval(80, 70, 20, 20, "red")
-    panel.fill_rect(80, 40, 30, 20, "cyan")
+def draw_car(p, x, y, size):
+    panel = DrawingPanel(260, 200, background="light gray")
+    panel.fill_rect(x, y, size, size/2, "black")
+    panel.fill_oval(x+size/10, y+size/10*4, size/5, size/5, "red")
+    panel.fill_oval(x+size/10*7, y+size/10*4, size/5, size/5, "red")
+    panel.fill_rect(x+size/10*7, y+size/10, size/10*3, size/5, "cyan")
 
 
 def my_function_top():
+    panel = DrawingPanel(260, 200, background="light gray")
     for i in range(0, 20):
         panel.draw_rectangle(20, 20+10*i, 200-10*i, 10)
 
 
 def my_function_left():
+    panel = DrawingPanel(260, 200, background="light gray")
     for i in range(0, 10):
         panel.draw_rectangle(20+10*i, 20+10 * i, 100-10*i, 10)
 
+
 def my_function_bottom():
+    panel = DrawingPanel(260, 200, background="light gray")
     for i in range(0, 10):
         panel.draw_rectangle(110-10*i, 20+10 * i, 10+10*i, 10)
-        
-my_function_bottom()
+
+
+def cars():
+    panel = DrawingPanel(260, 200, background="light gray")
+    draw_car(panel, 10, 30, 100)
+    draw_car(panel, 150, 10, 50)
+    for i in range(0, 260, 50):
+        draw_car(panel, 10+i, 130, 40)
+
+
+def animate(NUM_CIRCLES):
+    panel = DrawingPanel(250, 250)
+    for i in range(0, NUM_CIRCLES):
+        panel.draw_oval(15*i, 15*i, 40, 40)
+        panel.sleep(250)
+        panel.draw_oval(250-15*i, 15*i, 40, 40)
+        panel.sleep(250)
+        panel.draw_oval(15*i,250- 15*i, 40, 40)
+        panel.sleep(250)
+        panel.draw_oval(250-15*i,250- 15*i, 40, 40)
+        panel.sleep(100)
+
+animate(14)
