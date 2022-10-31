dea029de298ff8e6380961a04d11af67d8389c52
wiserenn <akincilareren@gmail.com>
2022-10-31 15:08:14 +0300

    modified:   mine/seven_game.py

--- mine/seven_game.py
+++ mine/seven_game.py
@@ -3,9 +3,9 @@ import time
 
 def seven_game():
     def create_player_list():
-        player_list = [*range(1, int(input("enter number of players :"))+1, 1)]
+        player_list = [*range(1, int(input("enter number of players :"))+1, 1)] #[1 2 3]
         for index in range(0, len(player_list)):
-            player_list[index] = input("Enter player name : ")
+            player_list[index] = input("Enter player name : ") #[eren,ayşe,elif]
         return player_list
 
     
@@ -14,13 +14,13 @@ def seven_game():
 
     # initialize first values
     for i in range(0, number_of_players):
-        player_list_in = [*range(0, number_of_players, 1)]
+        player_list_in = [*range(0, number_of_players, 1)] # num 3  [0,1,2]
         for index in range(0, number_of_players):
             player_list_in[index] = "player"+str(index+1)
     winner_not_found = True
     play_count = 0
     while (winner_not_found):
-        for index in range(0, number_of_players):
+        for index in range(0, number_of_players): #0 1 2
             count = 0
             play_count += 1
             time.sleep(0.01)
@@ -32,8 +32,10 @@ def seven_game():
                     count += 1
             print("|")
             if (count > 2):
-                print("Total Play count : ", play_count)
+                print("\nTotal Play count : ", play_count)
                 print("Play count of winner : ", play_count//number_of_players)
                 print(player_list[index], "Wins !!!")
                 winner_not_found = False
                 break
+
+seven_game()
